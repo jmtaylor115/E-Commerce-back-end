9b4019e50630eabd1ee1bd714b272dfad5298ad6
jmtaylor115 <jmtaylor115@yahoo.com>
2021-03-21 12:56:08 -0400

    updated tag routes for finding one and all tags, as well as creating, updating, and destroying a tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      association: [Product]
+    }]
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      association: [Product]
+    }]
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.category_name)
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
